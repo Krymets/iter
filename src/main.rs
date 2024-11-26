934999b0397b0050e5c78c2cbf09c5270bb23b84
Alexander <akrimec@gmail.com>
2024-11-26 14:52:14 +0200

    iter end

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,24 @@ fn to_uppercase(elements: & [String]) -> Vec<String> {
         .collect::<Vec<String>>()
 }
 
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|el| vec_b.push(el));
+}
+
+fn explode(elements: &[String]) -> Vec<Vec<String>> {
+    elements
+        .iter()
+        .map(|el|el.chars().map(|c| c.to_string()).collect())
+        .collect()
+}
+
+fn find_color_or(elements: &[String], search: &str, fallback: &str) -> String {
+    elements
+        .iter()
+        .find(|el| el.contains(search))
+        .map_or(String::from(fallback), |el| el.to_string())
+}
+
 fn main() {
     let mut colors = vec![
         String::from("red"),
@@ -45,4 +63,16 @@ fn main() {
 
     let uppercased = to_uppercase(&colors);
     println!("{:#?}", uppercased);
+
+    let mut destination = vec![];
+    move_elements(uppercased, &mut destination);
+    println!("Destination: {:#?}", destination);
+
+    let exploded = explode(&destination);
+    println!("{:#?}", exploded);
+
+    let found_color = find_color_or(
+        &destination, "B", "orange");
+    println!("{:#?}", found_color);
+
 }
